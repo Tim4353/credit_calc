5300e5182c3f456c920390862957bb7fc9ced279
Tim4353 <166216311+Tim4353@users.noreply.github.com>
2024-04-18 14:43:50 +0300

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -14,44 +14,37 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = "precache-v5"; // Имя кэша - как хранилища
-const RUNTIME = "runtime";
+const PRECACHE = 'precache-v5'; // Имя кэша - как хранилища
+const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 // Что мы хотим кэшить
-const PRECACHE_URLS = ["index.html"];
+const PRECACHE_URLS = [
+  'index.html'
+];
 //  './', // Alias for index.html
 // The install handler takes care of precaching the resources we always need.
 // Обработчик событий при установке приложения - он должен записать все в кэш заранее
-self.addEventListener("install", (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches
-      .open(PRECACHE)
-      .then((cache) => cache.addAll(PRECACHE_URLS))
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
       .then(self.skipWaiting())
   );
 });
 
 // The activate handler takes care of cleaning up old caches.
 // При активации сервис-воркера мы чистим старый кэш
-self.addEventListener("activate", (event) => {
+self.addEventListener('activate', event => {
   const currentCaches = [PRECACHE, RUNTIME];
   event.waitUntil(
-    caches
-      .keys()
-      .then((cacheNames) => {
-        return cacheNames.filter(
-          (cacheName) => !currentCaches.includes(cacheName)
-        );
-      })
-      .then((cachesToDelete) => {
-        return Promise.all(
-          cachesToDelete.map((cacheToDelete) => {
-            return caches.delete(cacheToDelete);
-          })
-        );
-      })
-      .then(() => self.clients.claim())
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    }).then(() => self.clients.claim())
   );
 });
 
@@ -59,17 +52,17 @@ self.addEventListener("activate", (event) => {
 // If no response is found, it populates the runtime cache with the response
 // from the network before returning it to the page.
 // Обработчик веб-запросов
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', event => {
   // Skip cross-origin requests, like those for Google Analytics.
   if (event.request.url.startsWith(self.location.origin)) {
     event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
+      caches.match(event.request).then(cachedResponse => {
         if (cachedResponse) {
           return cachedResponse;
         }
 
-        return caches.open(RUNTIME).then((cache) => {
-          return fetch(event.request).then((response) => {
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
             // Put a copy of the response in the runtime cache.
             return cache.put(event.request, response.clone()).then(() => {
               return response;
